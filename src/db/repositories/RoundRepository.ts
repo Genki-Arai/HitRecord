2d9c107e3ae5a8a909c7848c476758493c924b43
Genki Arai <genki@GenkinoMac-mini.local>
2026-05-02 16:29:38 +0900

    RoundRepositoryの実装

---
+++ src/db/repositories/RoundRepository.ts
@@ -0,0 +1,53 @@
+import { DatabaseManager } from "../DatabaseManager";
+import { RoundDataType } from "./RoundRepository.type";
+
+export class RoundRepository {
+  static async createRound(
+    session_id: number,
+    total_arrows: number,
+    bow_id?: number,
+    position_index?: number,
+  ): Promise<number> {
+    const db = await DatabaseManager.getDatabase();
+
+    const result = await db.runAsync(
+      `INSERT INTO rounds (session_id, bow_id, position_index, total_arrows) VALUES (?, ?, ?, ?)`,
+      [session_id, bow_id ?? null, position_index ?? null, total_arrows],
+    );
+    return result.lastInsertRowId;
+  }
+
+  static async getRoundsBySessionId(
+    sessionId: number,
+  ): Promise<RoundDataType[]> {
+    const db = await DatabaseManager.getDatabase();
+    return await db.getAllAsync(
+      `SELECT * FROM rounds WHERE session_id = ? AND is_deleted = 0`,
+      [sessionId],
+    );
+  }
+
+  static async getRoundById(roundId: number): Promise<RoundDataType | null> {
+    const db = await DatabaseManager.getDatabase();
+    return await db.getFirstAsync(
+      `SELECT * FROM rounds WHERE id = ? AND is_deleted = 0`,
+      [roundId],
+    );
+  }
+
+  static async updateKaichu(roundId: number, isKaichu: number): Promise<void> {
+    const db = await DatabaseManager.getDatabase();
+    await db.runAsync(`UPDATE rounds SET is_kaichu = ? WHERE id = ?`, [
+      isKaichu,
+      roundId,
+    ]);
+  }
+
+  static async deleteRound(roundId: number): Promise<void> {
+    // 物理削除ではなく論理削除を行う
+    const db = await DatabaseManager.getDatabase();
+    await db.runAsync(`UPDATE rounds SET is_deleted = 1 WHERE id = ?`, [
+      roundId,
+    ]);
+  }
+}
