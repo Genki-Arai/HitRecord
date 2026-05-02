25f7c79aad0c2f5e4d3c69972418e200d825ee0a
Genki Arai <genki@GenkinoMac-mini.local>
2026-05-02 22:51:09 +0900

    EquipmentRepositoryの実装

---
+++ src/db/repositories/EquipmentRepository.type.ts
@@ -0,0 +1,10 @@
+export type EquipmentDataType = {
+    id?: number;
+    user_id: string;
+    type: 'bow' | 'arrow' | 'string';
+    brand_name?: string;
+    weight?: number; // 弓の強さ (kg)。弓以外はNULL可
+    shaft_material?: string; // 矢の素材（例: カーボン、アルミなど）。弓や弦はNULL可
+    length_cm?: number; // 矢の長さ（cm）。弓や弦はNULL可
+    is_active?: number; // 1: 使用中, 0: 非アクティブ
+}
